ebda904d1ec067299336b0b665f4f2ebe9c064a6
Souldels <sanchdel@mail.ru>
2022-12-07 20:49:10 +0300

    created version for github pages

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Products from './Pages/Products';
 import Cart from './Pages/Cart';
 import NotFound from './Pages/NotFound';
 import Product from './Pages/Product';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import './reset.css';
 import './App.css';
 
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route exact path='/' element={<Login />} />
         <Route path='/menu' element={<Products />} />
         <Route path='/cart' element={<Cart />} />
         <Route path='*' element={<NotFound />} />
         <Route path='/product/:id' element={<Product />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
